4a39dd402115bb197a4722e2a0f500bc9644ece9
ak <andreinew@mail.ru>
2022-04-01 17:06:22 +0300

    chore: setup simple express server

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req, res)=>res.send('API Running'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
