e988c8c5b9cce95605cf3b0073f7549ae9ad73ce
AndreyKonovalenko <andreinew@mail.ru>
2022-04-23 20:41:30 +0300

    chore: work on logic

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,39 +1,19 @@
 import { StyledCard } from './styles/Card.styled';
-import { useState } from 'react';
 import { ListElem } from './styles/ListElem.styled';
-import data from '../__mocks__/questions';
+
 import uniqid from 'uniqid';
 import shuffle from '../utils/shuffle';
 
-const Card = () => {
-  const quiz = shuffle(data);
-  console.log(quiz.length)
-  const [quest, setQuest] = useState(quiz[0]);
-
-  //
-  const selectAnswer = (data, quest) => {
-    if (data !== undefined) {
-      const index = data.indexOf(quest);
-      if (index !== data.length - 1) {
-        setQuest(data[index + 1]);
-      }
-    }
-  };
-
-  const onClickHundler = (event) => {
-    event.preventDefault();
-    console.log('clicked');
-    selectAnswer(quiz, quest);
-  };
-
-  const list = shuffle(quest.answers).map((element) => (
-    <ListElem key={uniqid()} onClick={onClickHundler}>
+const Card = (props) => {
+  const { question, answers } = props.item;
+  const list = shuffle(answers).map((element) => (
+    <ListElem key={uniqid()} onClick={props.onClick}>
       <p>{element}</p>
     </ListElem>
   ));
   return (
     <StyledCard>
-      <h2>{quest.question}</h2>
+      <h2>{question}</h2>
       <ul>{list}</ul>
     </StyledCard>
   );
--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -1,8 +1,69 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { Button } from '../components/styles/Button.styled';
 import { Helmet } from 'react-helmet';
 import Card from '../components/Card';
+import shuffle from '../utils/shuffle';
+import data from '../__mocks__/questions';
 
 const Quiz = () => {
+  //Local state
+  const [inProgress, setInProgress] = useState(false);
+  const [quiz, setQuiz] = useState(undefined);
+  const [qIndex, setQIndex] = useState(0);
+  const [log, setLog] = useState([]);
+  const [result, setResult] = useState(0);
+  console.log(quiz);
+
+  // Event handlers
+
+  const onClickHundler = (event) => {
+    event.preventDefault();
+    if (qIndex < quiz.length - 1) {
+      setQIndex(qIndex + 1);
+    }
+    if (qIndex === quiz.length - 1) {
+      calculator(quiz, log);
+      setInProgress(false);
+    }
+    setLog((oldState) => [...oldState, event.target.innerText]);
+    console.log('clicked', qIndex);
+  };
+
+  const tryAgain = (event) => {
+    event.preventDefault();
+    setQIndex(0);
+    setResult(0);
+    setQuiz(shuffle(data));
+    setInProgress(true);
+  };
+  // utils
+  const calculator = (data, log) => {
+    let result = 0;
+    data.forEach((element, index) => {
+      //  console.log(element.answer, element.id);
+      if (element.answer === log[index]) {
+        result++;
+      }
+    });
+    setResult(result);
+  };
+
+  // Effect Hooks -----------------------------------------
+
+  useEffect(() => {
+    setQuiz(shuffle(data));
+    setInProgress(true);
+  }, [quiz, qIndex]);
+
+  const resultField = (
+    <>
+      <h2>Ваш результат: {result}</h2>
+      <Button onClick={tryAgain}>Повторить</Button>
+      <Button>Пeчатоть результат</Button>
+    </>
+  );
+
   return (
     <>
       <Helmet>
@@ -10,7 +71,11 @@ const Quiz = () => {
         <title>Quiz | Examinator </title>
       </Helmet>
       <h1>Quiz Page</h1>
-      <Card />
+      {quiz !== undefined && inProgress === true ? (
+        <Card item={quiz[qIndex]} onClick={onClickHundler} />
+      ) : null}
+
+      {inProgress === true ? null : resultField}
     </>
   );
 };
