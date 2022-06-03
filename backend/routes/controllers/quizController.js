3bb9f47fd6e1ecdc97d11ced458b822f373de4a8
ak <andreinew@mail.ru>
2022-06-03 16:06:00 +0300

    chore: added express async handler

--- backend/routes/controllers/quizController.js
+++ backend/routes/controllers/quizController.js
@@ -1,17 +1,18 @@
+import asyncHandler from 'express-async-handler'
 // @desc Get Quizzes
 // @route GET /api/quiz
 // @access Private
 
-const getQuizzes = (req, res) => {
+const getQuizzes = asyncHandler(async (req, res) => {
   res.status(200).json({ massage: 'GET ALL Quizzes' })
-}
+})
 
 // @desc Get Quizz
 // @route GET /api/quiz:id
 // @access Private
 
-const getQuiz = (req, res) => {
+const getQuiz = asyncHandler(async (req, res) => {
   res.status(200).json({ massage: 'GET QUIZ by ID' })
-}
+})
 
 export default { getQuizzes, getQuiz }
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,20 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import router from './routes/quizRoutes.js'
+import errorHandler from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.use('/api/quiz/', router)
 
 // app.get('/', (req, res) => res.status(200).json({ massage: 'API Running' }))
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000
 
