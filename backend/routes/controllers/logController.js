05887ea2f61164fcc8386f602b03eaca37640e71
AndreyKonovalenko <andreinew@mail.ru>
2022-06-13 14:51:26 +0300

    chore: work on result logic

--- backend/routes/controllers/logController.js
+++ backend/routes/controllers/logController.js
@@ -24,19 +24,51 @@ export const setLog = asyncHandler(async (req, res) => {
   const currentQuiz = await Quiz.findOne({ _id: quizId })
     .populate('questions')
     .exec();
-
-  console.log(currentQuiz);
-  const currect = 'result need to be calulated';
-  const newLog = await Log.create({
-    user: req.user.id,
-    quiz: quizId,
-    answers,
-    result: currect,
-  });
-  if (newLog) {
-    res.status(200).json(newLog);
-  } else {
-    res.status(400);
-    throw new Error('New Log has not been created');
+  if (currentQuiz) {
+    const { questions } = currentQuiz;
+    const result = await culcResult(questions, answers);
+  }
+  if (result) {
+    const newLog = await Log.create({
+      user: req.user.id,
+      quiz: quizId,
+      answers,
+      result: currect,
+    });
+    if (newLog) {
+      res.status(200).json(newLog);
+    } else {
+      res.status(400);
+      throw new Error('New Log has not been created');
+    }
   }
 });
+
+// util fuctions
+const arrayEquals = (a, b) => {
+  return (
+    Array.isArray(a) &&
+    Array.isArray(b) &&
+    a.length === b.length &&
+    a.every((val, index) => val === b[index])
+  );
+};
+
+// answers schema must be
+// [
+//   {qId:_id, answer:[String]]}
+// ]
+
+// culculate Result function
+const culcResult = (questions, answers) => {
+  let result = 0;
+  questions.forEach((element) => {
+    const { _id, currect } = element;
+    answers.forEach((el) => {
+      if (el.qId === _id) {
+        arrayEquals(currect, el.answer) ? (result += 1) : result;
+      }
+    });
+  });
+  return result;
+};
