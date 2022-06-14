ffb5f4552a9d52fcd5205780a248cd3e8a833fc1
ak <andreinew@mail.ru>
2022-06-14 13:45:55 +0300

    chore: work on authSlice

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,16 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+
+// Get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'))
 
 const initialState = {
-  user: {
-    name: 'Konovalenko Andrey Ivanovich',
-    offic: 'head of economical security department',
-    admin: true,
-  },
-};
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+}
 
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {},
-});
-export default authSlice.reducer;
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false
+      state.isSuccess = false
+      state.isError = false
+    },
+  },
+  extraREducers: () => {},
+})
+export default authSlice.reducer
