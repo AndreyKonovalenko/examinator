c6125783b0beb6a469e2b43decaa43b5909360dc
AndreyKonovalenko <andreinew@mail.ru>
2022-06-21 23:09:52 +0300

    chore: style update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { StyledFooter } from './styles/Footer.styled.js';
 const Footer = () => {
   return (
     <StyledFooter>
-      <h6>Created by a.k</h6>
+      <h6>2022 created by a.k</h6>
     </StyledFooter>
   );
 };
