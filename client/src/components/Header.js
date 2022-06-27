3914a362bb52ef26ccd643481385e5f3b7190ef6
ak <andreinew@mail.ru>
2022-06-27 12:11:28 +0300

    chore: work on reload logic

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,45 +1,47 @@
-import jwt_decode from 'jwt-decode';
-import { useEffect } from 'react';
-import { Container } from './styles/Container.styled';
-import { StyledHeader, StyledNav } from './styles/Header.styled';
-import { Button } from './styles/Button.styled';
-import { useNavigate, useLocation } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { logout, reset } from '../features/auth/authSlice';
-import { resetLogState } from '../features/log/logSlice';
-import { resetQuizState } from '../features/quiz/quizSlice';
+import jwt_decode from 'jwt-decode'
+import { useBeforeunload } from 'react-beforeunload'
+import { useEffect } from 'react'
+import { Container } from './styles/Container.styled'
+import { StyledHeader, StyledNav } from './styles/Header.styled'
+import { Button } from './styles/Button.styled'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { logout, reset } from '../features/auth/authSlice'
+import { resetLogState } from '../features/log/logSlice'
+import { resetQuizState } from '../features/quiz/quizSlice'
 
 const Header = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  let location = useLocation();
-  const { user } = useSelector((state) => state.auth);
+  useBeforeunload(() => 'You’ll lose your data!')
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  let location = useLocation()
+  const { user } = useSelector((state) => state.auth)
 
   const onLogout = () => {
-    dispatch(logout());
-    dispatch(resetLogState());
-    dispatch(resetQuizState());
-    dispatch(reset());
-    navigate('/login');
-  };
+    dispatch(logout())
+    dispatch(resetLogState())
+    dispatch(resetQuizState())
+    dispatch(reset())
+    navigate('/login')
+  }
 
   const onDashboard = () => {
-    dispatch(resetLogState());
-    dispatch(resetQuizState());
-    navigate('/');
-  };
+    dispatch(resetLogState())
+    dispatch(resetQuizState())
+    navigate('/')
+  }
   useEffect(() => {
     if (user) {
-      const decoded = jwt_decode(user.token);
+      const decoded = jwt_decode(user.token)
       if (decoded.exp * 1000 < Date.now()) {
-        dispatch(logout());
-        dispatch(reset());
-        dispatch(resetLogState());
-        dispatch(resetQuizState());
-        navigate('/login');
+        dispatch(logout())
+        dispatch(reset())
+        dispatch(resetLogState())
+        dispatch(resetQuizState())
+        navigate('/login')
       }
     }
-  });
+  }, [user])
 
   return (
     <StyledHeader>
@@ -53,6 +55,6 @@ const Header = () => {
         </StyledNav>
       </Container>
     </StyledHeader>
-  );
-};
-export default Header;
+  )
+}
+export default Header
