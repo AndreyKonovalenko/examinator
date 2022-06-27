f81db8bb53de603a1d80b454b9ebd0434a44a2d3
AndreyKonovalenko <andreinew@mail.ru>
2022-06-27 23:23:11 +0300

    chore work on summary reload issue

--- client/src/components/LogListCard.js
+++ client/src/components/LogListCard.js
@@ -1,46 +1,45 @@
-import moment from 'moment'
-import { StyledQuizCard } from './styles/QuizCard.styled'
-import { ListElem } from './styles/ListElem.styled'
-import theme from '../theme/index.js'
-import uniqid from 'uniqid'
+import moment from 'moment';
+import { StyledQuizCard } from './styles/QuizCard.styled';
+import { ListElem } from './styles/ListElem.styled';
+import theme from '../theme/index.js';
+import uniqid from 'uniqid';
 const LogListCard = (props) => {
   const list = props.item.map((element) => {
     const score = (
       (Number.parseInt(element.result) /
         Number.parseInt(element.quiz.questions.length)) *
       100
-    ).toFixed(0)
-    const etemptTime = moment(element.updatedAt).format('HH:mm:ss DD.MM.YYYY')
+    ).toFixed(0);
+    const etemptTime = moment(element.updatedAt).format('HH:mm:ss DD.MM.YYYY');
 
     const succes = {
       color: theme.colors.primary.light,
-    }
+    };
     const fail = {
       color: theme.colors.error,
-    }
+    };
     return (
       <ListElem
         key={uniqid()}
-        onClick={(event) => props.onClick(element, event)}
-      >
+        onClick={(event) => props.onClick(element, event)}>
         <p>
           {element.quiz.title}. Попытка осуществлена в {etemptTime}
         </p>
         <p style={score >= 80 ? succes : null}>
-          Тест {score >= 80 ? 'пройден' : 'провален'} с результатом {score}%,
-          правлельных ответов: {element.result} из{' '}
+          Тест {score >= 80 ? 'пройден успешно' : 'провален'} с результатом{' '}
+          {score}%, правлельных ответов: {element.result} из{' '}
           {element.quiz.questions.length}.
         </p>
       </ListElem>
-    )
-  })
+    );
+  });
 
   return (
     <StyledQuizCard>
       <h2>История:</h2>
       {list}
     </StyledQuizCard>
-  )
-}
+  );
+};
 
-export default LogListCard
+export default LogListCard;
