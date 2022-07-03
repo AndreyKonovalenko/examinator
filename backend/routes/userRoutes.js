972cbe3dd443500f9382411e0a074efa6e8ad309
AndreyKonovalenko <andreinew@mail.ru>
2022-07-03 15:20:23 +0300

    chore: enabled registration new users for dev verstion

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { login, getMe } from './controllers/userController.js';
+import { login, getMe, registerUser } from './controllers/userController.js';
 import protect from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // get all quizzes
-//router.post('/', registerUser)
+router.post('/', registerUser);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 
