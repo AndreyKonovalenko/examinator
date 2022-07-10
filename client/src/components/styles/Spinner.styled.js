4e546b7d8c48df6be5119c8573f21cadc5de0641
AndreyKonovalenko <andreinew@mail.ru>
2022-07-10 19:46:47 +0300

    chore: change Spinnor position

--- client/src/components/styles/Spinner.styled.js
+++ client/src/components/styles/Spinner.styled.js
@@ -1,17 +1,14 @@
 import styled from 'styled-components';
 
 export const SpinnerStyled = styled.div`
-  text-align: center;
-  margin-bottom: 3%;
+  position: fixed;
+  z-index: 999;
+  height: 2em;
+  width: 2em;
+  overflow: visible;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 `;
-
-// position: fixed;
-// z-index: 999;
-// height: 2em;
-// width: 2em;
-// overflow: visible;
-// margin: auto;
-// top: 0;
-// left: 0;
-// bottom: 0;
-// right: 0;
