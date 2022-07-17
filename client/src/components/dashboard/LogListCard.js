f948b14f4658957b7dfdb872c4c713b6eaba4395
AndreyKonovalenko <andreinew@mail.ru>
2022-07-17 19:26:07 +0300

    chore: work on chechbox styling

--- client/src/components/dashboard/LogListCard.js
+++ client/src/components/dashboard/LogListCard.js
@@ -1,8 +1,11 @@
-import moment from "moment";
-import { StyledListCard } from "../styles/ListCard.styled";
-import { ListElem } from "../styles/ListElem.styled";
-import theme from "../../theme/index.js";
-import uniqid from "uniqid";
+import moment from 'moment';
+import { StyledListCard } from '../styles/ListCard.styled';
+import { ListElem } from '../styles/ListElem.styled';
+import { IconStyled } from '../styles/Icon.styled';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import { Flex } from '../styles/Flex.styled';
+import theme from '../../theme/index.js';
+import uniqid from 'uniqid';
 
 const LogListCard = (props) => {
   const { en, ru } = props;
@@ -12,7 +15,7 @@ const LogListCard = (props) => {
         Number.parseInt(element.quiz.questions.length)) *
       100
     ).toFixed(0);
-    const etemptTime = moment(element.updatedAt).format("HH:mm:ss/DD.MM.YYYY");
+    const etemptTime = moment(element.updatedAt).format('HH:mm:ss/DD.MM.YYYY');
 
     const succes = {
       color: theme.colors.primary.light,
@@ -21,28 +24,36 @@ const LogListCard = (props) => {
       color: theme.colors.error,
     };
     return (
-      <ListElem
-        key={uniqid()}
-        onClick={(event) => props.onClick(element, event)}
-      >
-        {ru ? <p>Тема: {element.quiz.title}</p> : null}
-        {en ? <p>Quiz: {element.quiz.title}</p> : null}
-        {ru ? (
-          <p style={score >= 80 ? succes : fail}>
-            Тест {score >= 80 ? "пройден успешно" : "провален"} с результатом{" "}
-            {score}%, правильных ответов: {element.result} из{" "}
-            {element.quiz.questions.length}
-          </p>
-        ) : null}
-        {en ? (
-          <p style={score >= 80 ? succes : fail}>
-            {score >= 80 ? "You have succeeded" : "You have failed"} with{" "}
-            {score}%, correct answers: {element.result} out of{" "}
-            {element.quiz.questions.length}
-          </p>
-        ) : null}
-        <p>{etemptTime}</p>
-      </ListElem>
+      <>
+        <IconStyled>
+          <MdCheckBox />
+        </IconStyled>
+        <IconStyled>
+          <MdCheckBoxOutlineBlank />
+        </IconStyled>
+        <ListElem
+          key={uniqid()}
+          onClick={(event) => props.onClick(element, event)}>
+          {ru ? <p>Тема: {element.quiz.title}</p> : null}
+          {en ? <p>Quiz: {element.quiz.title}</p> : null}
+          {ru ? (
+            <p style={score >= 80 ? succes : fail}>
+              Тест {score >= 80 ? 'пройден успешно' : 'провален'} с результатом{' '}
+              {score}%, правильных ответов: {element.result} из{' '}
+              {element.quiz.questions.length}
+            </p>
+          ) : null}
+          {en ? (
+            <p style={score >= 80 ? succes : fail}>
+              {score >= 80 ? 'You have succeeded' : 'You have failed'} with{' '}
+              {score}%, correct answers: {element.result} out of{' '}
+              {element.quiz.questions.length}
+            </p>
+          ) : null}
+
+          <p>{etemptTime}</p>
+        </ListElem>
+      </>
     );
   });
 
