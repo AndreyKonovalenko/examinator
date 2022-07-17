cc14a2b8b72dcd1fec52845eca9c06c350718cd9
AndreyKonovalenko <andreinew@mail.ru>
2022-07-17 14:11:18 +0300

    chore: token logic

--- client/src/features/auth/authSlice.js
+++ client/src/features/auth/authSlice.js
@@ -1,20 +1,20 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import authService from "./authService";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
 
 // Get user from localStorage
-const user = JSON.parse(localStorage.getItem("user"));
+const user = JSON.parse(localStorage.getItem('user'));
 
 const initialState = {
   user: user ? user : null,
   isError: false,
   isSuccess: false,
   isLoading: false,
-  message: "",
+  message: '',
 };
 
 // Register user
 export const register = createAsyncThunk(
-  "auth/register",
+  'auth/register',
   async (user, thunkAPI) => {
     try {
       return await authService.register(user);
@@ -31,7 +31,7 @@ export const register = createAsyncThunk(
 );
 // Create new user / admin route
 export const createNewUser = createAsyncThunk(
-  "auth/createNewUser",
+  'auth/createNewUser',
   async (user, thunkAPI) => {
     try {
       return await authService.register(user);
@@ -47,12 +47,12 @@ export const createNewUser = createAsyncThunk(
   }
 );
 
-export const logout = createAsyncThunk("auth/logout", async () => {
+export const logout = createAsyncThunk('auth/logout', async () => {
   await authService.logout();
 });
 
 // login user
-export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
+export const login = createAsyncThunk('auth/login', async (user, thunkAPI) => {
   try {
     return await authService.login(user);
   } catch (error) {
@@ -65,14 +65,14 @@ export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
 });
 
 export const authSlice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState,
   reducers: {
     reset: (state) => {
       state.isLoading = false;
       state.isSuccess = false;
       state.isError = false;
-      state.message = "";
+      state.message = '';
     },
   },
   extraReducers: (builder) => {
