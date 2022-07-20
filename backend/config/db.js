b7f70964a11fee70cab4eeb79b1e6c65efb81913
AndreyKonovalenko <andreinew@mail.ru>
2022-07-20 16:35:39 +0300

    created second db prod

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,13 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      dbName: 'prod',
+    });
+    console.log(
+      `MongoDB Connected! db:${conn.connection.name}  host:${conn.connection.host}`
+        .cyan.underline
+    );
   } catch (error) {
     console.log(error);
     process.exit(1);
