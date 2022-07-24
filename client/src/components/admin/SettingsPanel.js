f2825c777ff6aa20c1daa1a6ae9ceab5e83636e4
AndreyKonovalenko <andreinew@mail.ru>
2022-07-24 19:00:51 +0300

    chore: added close button

--- client/src/components/admin/SettingsPanel.js
+++ client/src/components/admin/SettingsPanel.js
@@ -1,28 +1,30 @@
-import { IconStyled } from "../styles/Icon.styled";
-import { MdSettings, MdDelete } from "react-icons/md";
-import theme from "../../theme";
+import { IconStyled } from '../styles/Icon.styled';
+import { MdSettings, MdDelete, MdClose } from 'react-icons/md';
+import theme from '../../theme';
 
 const SettingPanel = (props) => {
   const { onSettings, isEdit, onDelete } = props;
   return (
-    <div style={{ textAlign: "right" }}>
+    <div style={{ textAlign: 'right' }}>
       <IconStyled
         bg={theme.colors.surface}
         color={theme.colors.primary.light}
-        onClick={onSettings}
-      >
-        <MdSettings size={"2em"} />
+        onClick={onSettings}>
+        <MdSettings size={'2em'} />
       </IconStyled>
+
       {isEdit ? (
         <IconStyled
           bg={theme.colors.surface}
           color={theme.colors.primary.light}
           hcolor={theme.colors.error}
-          onClick={onDelete}
-        >
-          <MdDelete size={"2em"} />
+          onClick={onDelete}>
+          <MdDelete size={'2em'} />
         </IconStyled>
       ) : null}
+      <IconStyled bg={theme.colors.surface} color={theme.colors.primary.light}>
+        <MdClose size={'2em'} />
+      </IconStyled>
     </div>
   );
 };
