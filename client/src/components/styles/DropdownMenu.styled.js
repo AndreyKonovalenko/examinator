22398b43687c7c2ba9e1aa1b37955f214a41f262
AndreyKonovalenko <andreinew@mail.ru>
2022-07-28 02:03:16 +0300

    chore: styling

--- client/src/components/styles/DropdownMenu.styled.js
+++ client/src/components/styles/DropdownMenu.styled.js
@@ -4,6 +4,7 @@ export const StyledDropdownMenu = styled.div`
   position: absolute;
   top: 58px;
   width: 300px;
+  height: 100px;
   transform: translateX(-45%);
   background-color: ${({ bg }) => bg || (({ theme }) => theme.colors.surface)};
   border: 1px solid black;
