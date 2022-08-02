3884365032f597a5040c56bfa2e7c97a439b8121
ak <andreinew@mail.ru>
2022-08-02 17:58:57 +0300

    chore: textarea styling

--- client/src/components/controls/Textarea.js
+++ client/src/components/controls/Textarea.js
@@ -1,19 +1,24 @@
-import { Button } from "../styles/Textarea.styled";
+import { Button, Conteiner, STextarea, Flex } from "../styles/Textarea.styled";
 
 const Textarea = (props) => {
   const { onSave, en, ru } = props;
   return (
-    <div>
-      <textarea />
-      <div>
+    <Conteiner>
+      <h3>
+        {ru ? "Придумайте тему нового тестирования" : null}
+        {en ? "Choose new quiz titel" : null}
+      </h3>
+
+      <STextarea />
+      <Flex style={{ justifyContent: "flex-end" }}>
         <Button onClick={() => onSave()}>
           {ru ? "Сохранить" : null} {en ? "Save" : null}
         </Button>
         <Button>
           {ru ? "Очистить" : null} {en ? "Clear" : null}
         </Button>
-      </div>
-    </div>
+      </Flex>
+    </Conteiner>
   );
 };
 
--- client/src/components/styles/Textarea.styled.js
+++ client/src/components/styles/Textarea.styled.js
@@ -1,4 +1,43 @@
 import styled from "styled-components";
+
+export const Conteiner = styled.div`
+  background-color: ${({ theme }) => theme.colors.surface};
+  color: ${({ theme }) => theme.colors.text.onSurface};
+  border-radius: 15px;
+  margin: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    color: ${({ theme }) => theme.colors.primary.main};
+    margin: 15px 0 0 0;
+  }
+`;
+export const STextarea = styled.textarea`
+  width: 95%;
+  max-width: 95%;
+  margin: 10px 0;
+  height: 5em;
+  font-size: 16px;
+  font-weight: 700;
+  margin-bottom: 10px;
+  background-color: ${({ theme }) => theme.colors.surface};
+  opacity: 0.5;
+  color: ${({ theme }) => theme.colors.primary.light};
+  &:hover {
+    opacity: 1;
+  }
+  &:active {
+    border-color: red;
+  }
+`;
+export const Flex = styled.div`
+  width: 95%;
+  display: flex;
+  justify-content: center;
+`;
+
 export const Button = styled.button`
   display: flex;
   align-items: center;
@@ -7,11 +46,11 @@ export const Button = styled.button`
   cursor: pointer;
   font-size: 16px;
   font-weight: 700;
-  padding: 0;
+  margin-bottom: 10px;
   background-color: ${({ theme }) => theme.colors.surface};
   color: ${({ theme }) => theme.colors.primary.light};
   &:hover {
     opacity: 0.5;
     transform: scale(0.98);
-    background-color: ${({ theme }) => theme.colors.primary.main};
+  }
 `;
