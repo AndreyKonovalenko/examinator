aea69d7d324166e4e26e7df08258d2518ce49b32
AndreyKonovalenko <andreinew@mail.ru>
2022-08-07 18:21:26 +0300

    chore: work on adding options to quiz logic

--- client/src/components/admin/AddQuestionModal.js
+++ client/src/components/admin/AddQuestionModal.js
@@ -1,6 +1,9 @@
+import { useState, useEffect } from 'react';
+import { Button } from '../styles/Textarea.styled';
 import { useDispatch } from 'react-redux';
 import Modal from '../controls/Modal';
 import Textarea from '../controls/Textarea';
+import uniqid from 'uniqid';
 
 import { setAddQuestionModalOff } from '../../features/ui/uiSlice';
 
@@ -9,17 +12,37 @@ const AddQuestionModal = (props) => {
 
   const { en, ru } = props;
 
+  const [optionslist, setOpitionList] = useState(2);
+
   const onSave = (data) => {
     console.log('question added');
   };
   const onClose = () => {
     dispatch(setAddQuestionModalOff());
   };
+  const list = (n) => {
+    const result = [];
+    let i = 0;
+    while (i < n) {
+      result.push(
+        <li style={{ listStyleType: 'none' }} key={uniqid()}>
+          <Textarea en={en} ru={ru} onSave={onSave} maxLength={100} />
+        </li>
+      );
+
+      i++;
+    }
+    return result;
+  };
+
+  const onAdd = () => setOpitionList(optionslist + 1);
+  useEffect(() => {}, []);
 
   return (
     <Modal onClose={onClose}>
-      <Textarea en={en} ru={ru} onSave={onSave} maxLength={100} />
-      <Textarea en={en} ru={ru} onSave={onSave} maxLength={100} />
+      <h3>options</h3>
+      {list(optionslist)}
+      <Button onClick={onAdd}>ADD</Button>
     </Modal>
   );
 };
