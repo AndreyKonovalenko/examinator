fff38232b072a3d69c430a09fc6650f0152e308e
AndreyKonovalenko <andreinew@mail.ru>
2022-08-22 23:41:04 +0300

    feat: Comment before global quiz logic changes, implementation of quiz version control system

--- backend/models/questionModel.js
+++ backend/models/questionModel.js
@@ -8,3 +8,7 @@ const questionSchema = new Schema({
 });
 
 export const Question = mongoose.model('question', questionSchema);
+// decided to create quiz version control system by adding timestamps to questions
+// as a version of the quiz, we will use an array of questions whose creation timestamp is less than the update timestamp of quiz
+// there is a logical assumption that the question update timestamp will not affect the final result quiz result,
+// it will not be taken into quiz version calculation
