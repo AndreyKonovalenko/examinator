82c2c89739cafd8c814ac39db9d658758bcff02c
AndreyKonovalenko <andreinew@mail.ru>
2022-09-18 20:45:45 +0300

    feat: fixed issue with two same error appearing on when jwt expired

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,57 +1,61 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router";
-import { Helmet } from "react-helmet";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import { Helmet } from 'react-helmet';
+import { useSelector, useDispatch } from 'react-redux';
 
-import QuizListCard from "../components/dashboard/QuizListCard";
-import LogListCard from "../components/dashboard/LogListCard";
-import Spinner from "../components/Spinner";
+import QuizListCard from '../components/dashboard/QuizListCard';
+import LogListCard from '../components/dashboard/LogListCard';
+import Spinner from '../components/Spinner';
 
-import { getQuizById } from "../features/quiz/quizSlice";
-import { getQuizzes } from "../features/quizzes/quizzesSlice";
-import { getLogById } from "../features/log/logSlice";
-import { getLogs } from "../features/logs/logsSlice";
-import { resetLogState } from "../features/log/logSlice";
-import { resetQuizState } from "../features/quiz/quizSlice";
+import { getQuizById } from '../features/quiz/quizSlice';
+import { getQuizzes } from '../features/quizzes/quizzesSlice';
+import { getLogById } from '../features/log/logSlice';
+import { getLogs } from '../features/logs/logsSlice';
+import { resetLogState } from '../features/log/logSlice';
+import { resetQuizState } from '../features/quiz/quizSlice';
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { ru, en } = useSelector((state) => state.ui);
   const { user } = useSelector((state) => state.auth);
-  const { quizzes, isLoading: quizzesIsLoading } = useSelector(
-    (state) => state.quizzes
-  );
+  const {
+    quizzes,
+    isLoading: quizzesIsLoading,
+    isSuccess: quizzesIsSucccse,
+  } = useSelector((state) => state.quizzes);
   const { logs, isLoading: logsIsLoading } = useSelector((state) => state.logs);
 
   useEffect(() => {
     if (!user) {
-      navigate("/login");
+      navigate('/login');
     }
     if (user) {
       dispatch(getQuizzes());
-      dispatch(getLogs());
+      if (quizzesIsSucccse) {
+        dispatch(getLogs());
+      }
       dispatch(resetLogState());
       dispatch(resetQuizState());
     }
-  }, [dispatch, navigate, user]);
+  }, [dispatch, navigate, user, quizzesIsSucccse]);
 
   const onQuizSelect = (id) => {
     if (id) {
       dispatch(getQuizById(id));
-      navigate("/quiz");
+      navigate('/quiz');
     }
   };
 
   const onLogHandler = (log) => {
     dispatch(getLogById(log._id));
-    navigate("/summary");
+    navigate('/summary');
   };
 
   const dashboard = (
     <>
       <Helmet>
-        <meta charSet="utf-8" />
+        <meta charSet='utf-8' />
         <title>Dashboard | Examinator</title>
       </Helmet>
 
