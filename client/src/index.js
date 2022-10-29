5ae40140ef2cc4c9a62df320602716c0131eaae2
AndreyKonovalenko <andreinew@mail.ru>
2022-10-29 18:48:46 +0300

    chore: added google tag manager

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const tagManagerArgs = {
+  gtmId: 'G-R882JJ95Q0',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
